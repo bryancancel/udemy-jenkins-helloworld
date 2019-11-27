b88517c64d176d87822dc3ff16c034020453314b
Bryan Cancel <bryan.cancel@slalom.com>
2019-11-27 00:17:08 -0600

    made some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("-----***** Hello World & Welcome to Jenkins Training *****-----");
+        System.out.println(HelloWorld.getMessage());
+    }
+    private static String getMessage(){
+		String message = "-----***** Hello World & Welcome to Jenkins Training *****-----";
+		message += "\nMade Some Changes";
+        return message;
     }
 }
