3ef636a70bdf6b0de732c7f999839d2e29ecabd0
Bryan Cancel <bryan.cancel@slalom.com>
2019-12-16 11:59:38 -0600

    moving changes to a java project structure

---
+++ HelloWorld/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import app.HelloWorld;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(HelloWorld.getMessage());
+    }
+}
---
+++ HelloWorld/src/app/HelloWorld.java
@@ -0,0 +1,12 @@
+package app;
+
+public class HelloWorld {
+    
+    public static String getMessage(){
+		String message = "-----***** Hello World & Welcome to Jenkins Training *****-----";
+		message += "\nMade Some Changes";
+        message += "\nMade More Changes";
+        return message;
+    }
+    
+}
